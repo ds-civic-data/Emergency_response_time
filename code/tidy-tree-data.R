85f0a5b6efbf7c1fe2b5c41960b3d05565c4f36c
Miranda A Rintoul <rintoulm@rstudio-dev.reed.edu>
2018-04-30 10:05:31 -0700

    update to tidy full dataset

--- code/tidy-tree-data.R
+++ code/tidy-tree-data.R
@@ -1,55 +1,70 @@
 library(tidyverse)
-# read data
-trees <- read.csv("~/emergency-response-time/data-raw/tree-data-13april.csv")
 
-# select useful columns
-trees <- trees %>%
-  select(X,Y,Year_Plant,Family,Common_nam,Genus_spec,Functional,
-         Size,Native,Edible,Nuisance,Origin,Planting_O)
+# read data
+trees <- read.csv("~/emergency-response-time/data/complete_tablejoin.csv")
 
 ########################################
-# transform data
-# make origin regex to pull out continents
-origin_pat <- "^[A-Z][a-z]+ ?[A]?[a-z]+"
-x <-grepl(origin_pat,trees$Origin)
-# remove bad origin rows, make better origin variable
-trees <- trees %>%
-  filter(x) %>%
-  mutate(origin_wip = grep(origin_pat,Origin, value=T) %>% str_extract(origin_pat)) %>%
-  mutate(origin = ifelse(origin_wip=="China","Asia",origin_wip)) %>%
-  select(-Origin,-origin_wip)
-
+# transform data (tree stuff)
 trees <- trees %>%
   # filter out trees with no native val, make better "native" col
   filter(Native %in% c("Yes","No")) %>%
   mutate(native=ifelse(Native=="Yes",T,F)) %>%
   select(-Native) %>%
-  # mutate better "group" col
-  mutate(group = ifelse(Planting_O=="BES Contractor", "contractor",
-                        ifelse(Planting_O=="FoTStreet","street",
-                               ifelse(Planting_O=="FoTYard","yard","park")))) %>%
-  select(-Planting_O) %>%
-  # mutate better "nuisance" col
-  mutate(nuisance=ifelse(Nuisance!="",T,F)) %>%
-  select(-Nuisance) %>% 
-  # mutate better "edible" col
-  mutate(edible=ifelse(Edible=="Yes - fruit","fruit",
-                       ifelse(Edible=="Yes - nuts","nuts","none"))) %>%
-  select(-Edible) %>%
   # mutate radius, area for canopy cover
   mutate(canopy_rad=ifelse(Size=="L", 60,
                            ifelse(Size=="M", 40, 20))) %>%
-  mutate(canopy_area = pi*canopy_rad^2)
+  mutate(canopy_area = pi*canopy_rad^2) %>%
+  # mutate cols for broadleaf/conifer and deciduous/evergreen
+  mutate(type=ifelse(Functional=="BD"|Functional=="CD", "deciduous", "evergreen")) %>%
+  select(-Functional)
 
 # reorder and rename cols
 trees <- trees %>%
-  select(X,Y,Year_Plant,Common_nam,Size,canopy_rad,canopy_area,native,
-         nuisance,edible,Family,Genus_spec,origin,Functional,group)
-names(trees) <- c("xcoord","ycoord","year","name","size","canopy_rad","canopy_area",
-                  "native","nuisance","edible","family","species","origin",
-                  "funct","group")
-########################################
+  select(Common_nam,Genus_spec,Year_Plant, Size, native, canopy_rad, canopy_area,
+         type, SE_T061_001,Geo_GEOID)
 
-# export new tidy as csv
-write.csv(trees, "~/emergency-response-time/data/tree_tidy.csv",row.names=T)
+names(trees) <- c("name","species","year","size","native","canopy_rad",
+                  "canopy_area","type","income","geoid")
+
+########################################
+# transform data (inome stuff)
+pdx_mean_inc <- 77208
+
+trees <- trees %>%
+  mutate(inc_level = ifelse(income>(.8*pdx_mean_inc), "normal",
+                            ifelse(income>(.5*pdx_mean_inc), "low", "very_low")))
+
+########################################
+# create tree index
+# get rid of cols we don't need
+index_build <- trees %>%
+  select(name, type, size, native)
+# mutate a whole bunch of numeric variables
+index_build <- trees %>%
+  mutate(s = ifelse(size=="S", 1,
+                    ifelse(size=="M", 3, 5))) %>%
+  mutate(n = ifelse(native, 1, 0)) %>%
+  mutate(e = ifelse(type=="evergreen",2, 0)) %>%
+  # mutate index
+  mutate(index = s + n + e)
+
+# make a simple index of all the species
+tree_index <- index_build %>%
+  select(name,index) %>%
+  group_by(name) %>%
+  summarize(index=mean(index))
+glimpse(tree_index)
+
+# full join index and tidy set
+tree_tidy_index <- trees %>%
+  full_join(tree_index,by=c("name"="name"))
+glimpse(tree_tidy_index)
+
+########################################
+# export new tidy table as csv
+write.csv(trees, "~/emergency-response-time/data/fulltable_tidy.csv",row.names=T)
+
+# export tree index
+write.csv(tree_index, 
+          "~/emergency-response-time/data/tree_index.csv",row.names=F)
 
