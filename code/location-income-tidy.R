95cde4322f25742e20f4d00de1c9302fa5e6ece7
Lirui Jiao <lirjiao@rstudio-dev.reed.edu>
2018-05-02 15:50:48 -0700

    initial commit

---
+++ code/location-income-tidy.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(raster)
+library(sp)
+
+
+# read spatial data
+setwd("~/emergency-response-time/data-raw/census-tracts")
+location <- shapefile("~/emergency-response-time/data-raw/census-tracts/tract2010.shp")
+
+# read income csv data
+medianincome <- read.csv("~/emergency-response-time/data-raw/median-income.csv")
+
+  names(medianincome)[names(medianincome) == 'Geo_FIPS'] <- 'FIPS'
+
+# merge 
+location_income <- merge(location, medianincome, by='FIPS')
+
+# perhaps save as shapefile again
+shapefile(location_income, "~/emergency-response-time/merged.shp")
