6f707f717e0a787a342ace0679d257b02bf3d304
Miranda A Rintoul <rintoulm@rstudio-dev.reed.edu>
2018-05-05 17:03:40 -0700

    export plots for initial analysis

--- code/tree-type-visuals.R
+++ code/tree-type-visuals.R
@@ -3,8 +3,15 @@ library(dplyr)
 
 trees <- read.csv("~/emergency-response-time/data/trees.csv")
 
+ind <- trees %>%
+  filter(year < 2018) %>%
+  ggplot(aes(x=year,fill=factor(index))) + 
+  geom_bar(position="fill") +
+  scale_fill_brewer(palette="RdYlGn") +
+  labs(title="Plantings by Tree Index",x="Year",y="Percent of Total Plantings",
+       fill="Index Value")
 
-trees %>%
+size <- trees %>%
   filter(year < 2018) %>%
   ggplot(aes(x=year,fill=factor(size,levels=c("S","M","L")))) + 
   geom_bar(position="fill") +
@@ -12,21 +19,31 @@ trees %>%
   labs(title="Planting Proportions by Size",x="Year",y="Percent of Total Plantings",
        fill="Size")
 
-trees %>%
+native <- trees %>%
   filter(year < 2018) %>%
   ggplot(aes(x=year,fill=factor(native,levels=c("FALSE", "TRUE")))) + 
   geom_bar(position="fill") +
-  scale_fill_brewer(palette="GnBu") +
-  labs(title="Planting Proportions by Native",x="Year",y="Percent of Native Species",
-       fill="Native")
+  scale_fill_manual(labels = c("Non-native","Native"),
+                    values=c("greenyellow","springgreen3")) +
+  labs(title="Native Plantings",x="Year",y="Percent of Native Species",
+       fill="Type")
 
 
-trees %>%
+ever <- trees %>%
   filter(year < 2018) %>%
   ggplot(aes(x=year,fill=factor(group,levels=c("deciduous", "evergreen")))) + 
   geom_bar(position="fill") +
   scale_fill_manual(labels = c("Deciduous","Evergreen"),
-                    values=c("yellow2","springgreen3")) +
-  labs(title="Evergreen Plantings",x="Year",y="Percent of Total Plantings", fill = "Group")
+                    values=c("greenyellow","springgreen3")) +
+  labs(title="Evergreen Plantings",x="Year",y="Percent of Total Plantings",
+       fill="Type")
+
+# export plots
+ggsave("index.png",ind, path="~/emergency-response-time/documents/visuals")
+ggsave("size.png",size, path="~/emergency-response-time/documents/visuals")
+ggsave("native.png",native, path="~/emergency-response-time/documents/visuals")
+ggsave("evergreen.png",ever, path="~/emergency-response-time/documents/visuals")
+
+
 
 
